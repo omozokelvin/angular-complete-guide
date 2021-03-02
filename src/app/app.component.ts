96206c00feee24e6a423c388529562f1c5df279b
Darkel Zezrucion <kelvinjune2@gmail.com>
2021-03-02 19:00:50 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isLoading = false;
+  loadedFeature = 'recipe';
 
-
+  onNavigate(feature: string): void {
+    this.loadedFeature = feature;
+  }
 }
