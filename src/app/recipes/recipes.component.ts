54fb20dcf27e636ef9a61a5c232139778f0f809d
Darkel Zezrucion <kelvinjune2@gmail.com>
2021-04-26 18:40:38 +0100

    added subjects instead of event emitters

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {RecipeService} from './recipe.service';
-import {Recipe} from './recipe.model';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
@@ -9,16 +8,12 @@ import {Recipe} from './recipe.model';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected
-      .subscribe((recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      });
+
   }
 
 }
